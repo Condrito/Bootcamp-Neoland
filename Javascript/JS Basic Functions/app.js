524259e08687845e3d384986b84473308c4ccbce
Condrito <mareyeslacalle@gmail.com>
2022-10-06 23:18:10 +0200

    iteracion 6 basic functions

--- Javascript/JS Basic Functions/app.js
+++ Javascript/JS Basic Functions/app.js
@@ -140,3 +140,58 @@ const averageWord = (elements) => {
 };
 console.log(averageWord(mixedElements));
 
+
+
+/*----------------------------------------------------------------------------------
+-------------------------**Iteración #6: Valores únicos**---------------------------
+------------------------------------------------------------------------------------*/
+
+
+/*Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados,
+ en caso que existan los elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar tu función:
+
+const duplicates = [
+  'sushi',
+  'pizza',
+  'burger',
+  'potatoe',
+  'pasta',
+  'ice-cream',
+  'pizza',
+  'chicken',
+  'onion rings',
+  'pasta',
+  'soda'
+];
+function removeDuplicates(param) {
+  // insert code
+}
+*/
+
+const duplicates = [
+  'sushi',
+  'pizza',
+  'burger',
+  'potatoe',
+  'pasta',
+  'ice-cream',
+  'pizza',
+  'chicken',
+  'onion rings',
+  'pasta',
+  'soda'
+];
+
+const removeDuplicates = (foods) => {
+  let uniquefoods = [];
+  for (let i = 0; i < foods.length; i++) {
+    if (uniquefoods.includes(foods[i])){
+    } else {(uniquefoods.push(foods[i]))}
+    }
+  return uniquefoods;
+  }
+
+
+console.log(removeDuplicates(duplicates));
+
+
