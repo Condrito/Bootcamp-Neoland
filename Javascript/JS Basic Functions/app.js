424f9695f0c73685cfc4e8dbd4367372334b0838
Condrito <mareyeslacalle@gmail.com>
2022-10-06 20:27:37 +0200

    iteracion 5 Basic Functions

--- Javascript/JS Basic Functions/app.js
+++ Javascript/JS Basic Functions/app.js
@@ -84,12 +84,9 @@ function sumAll(array) {
 }
 console.log(sumAll(numbers));
 
-
-
 /*----------------------------------------------------------------------------------
 -----------------------**Iteración #4: Calcular el promedio**-----------------------
 ------------------------------------------------------------------------------------*/
-
 
 /*Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función:
 
@@ -103,12 +100,43 @@ function average(param) {
 const numbers2 = [12, 21, 38, 5, 45, 37, 6];
 function average(array) {
   let suma = 0;
- 
-  for(let i = 0; i < array.length; i++) {
-  suma += array[i];
+
+  for (let i = 0; i < array.length; i++) {
+    suma += array[i];
   }
   let promedio = suma / array.length;
   return promedio;
 }
 
 console.log(average(numbers2));
+
+/*----------------------------------------------------------------------------------
+------------------**Iteración #5: Calcular promedio de strings**--------------------
+------------------------------------------------------------------------------------*/
+
+/*Crea una función que reciba por parámetro un array y cuando es un valor number lo sume 
+y de lo contrario cuente la longitud del string y lo sume. Puedes usar este array para probar tu función:
+
+```jsx
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+function averageWord(param) {
+  // insert code
+}
+```*/
+
+const mixedElements = [6, 1, "Rayo", 1, "vallecano", "10", "upgrade", 8, "hub"];
+
+const averageWord = (elements) => {
+  let sumElements = 0;
+  for (let element of elements) {
+    if (typeof element == "number") {
+      sumElements += element;
+    }
+    if (typeof element == "string") {
+      sumElements += element.length;
+    }
+  }
+  return sumElements;
+};
+console.log(averageWord(mixedElements));
+
