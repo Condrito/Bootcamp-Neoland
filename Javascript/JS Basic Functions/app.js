76be8786c218f046ba3f47136ff4656ec82f1cd0
Condrito <mareyeslacalle@gmail.com>
2022-10-07 21:04:29 +0200

    iteracion 7 Basic Functions

--- Javascript/JS Basic Functions/app.js
+++ Javascript/JS Basic Functions/app.js
@@ -195,3 +195,114 @@ const removeDuplicates = (foods) => {
 console.log(removeDuplicates(duplicates));
 
 
+
+
+/*----------------------------------------------------------------------------------
+-------------------------**Iteración #7: Buscador de nombres**----------------------
+------------------------------------------------------------------------------------*/
+
+
+/*Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array
+ - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false.
+  Puedes usar este array para probar tu función:
+
+const nameFinder = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+];
+function finderName(param) {
+  // insert code
+}
+*/
+
+const nameFinder = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+];
+
+
+/*
+const finderName = (array, value) => {
+let elementExist = array.includes(value);
+let element = array.indexOf(value);
+  if (elementExist === true) {
+    console.log(elementExist, element);
+  } else {
+    console.log(elementExist)
+  }
+}
+
+finderName(nameFinder, "Bruce");
+
+*/
+
+const finderName = (array, value) => {
+let elementExist = array.includes(value);
+let element = array.indexOf(value);
+elementExist == true ? console.log(elementExist, element) : console.log(elementExist);
+}
+
+finderName(nameFinder, "Bruce");
+
+
+
+/*----------------------------------------------------------------------------------
+-----------------------**Iteration #8: Contador de repeticiones**--------------------
+------------------------------------------------------------------------------------*/
+
+
+/*Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.
+Puedes usar este array para probar tu función:
+
+
+const counterWords = [
+  'code',
+  'repeat',
+  'eat',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code',
+  'enjoy',
+  'upgrade',
+  'code'
+];
+function repeatCounter(param) {
+  // insert code
+}
+*/
+
+const counterWords = [
+  'code',
+  'repeat',
+  'eat',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code',
+  'enjoy',
+  'upgrade',
+  'code'
+];
+
+
