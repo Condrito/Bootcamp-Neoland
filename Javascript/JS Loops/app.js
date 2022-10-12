6dd0fa7f77179c66fdfb86f0541cea68dc5f5e83
Condrito <mareyeslacalle@gmail.com>
2022-10-12 19:17:30 +0200

    iteracion 7 Loops

--- Javascript/JS Loops/app.js
+++ Javascript/JS Loops/app.js
@@ -194,11 +194,9 @@ for (let i = 0; i < placesToTravel2.length; i++) {
 }
 console.log(placesTavel);
 
-
 /*----------------------------------------------------------------------------------
 -------------------**Iteración #6: Mixed For...of e includes**----------------------
 ------------------------------------------------------------------------------------*/
-
 
 /*Usa un bucle **for...of** para recorrer todos los juguetes y elimina los que incluyan la palabra gato.
 Recuerda que puedes usar la función ***.includes()*** para comprobarlo.Puedes usar este array:
@@ -214,20 +212,56 @@ const toys = [
 */
 
 const toys = [
-  {id: 5, name: 'Buzz MyYear'}, 
-  {id: 11, name: 'Action Woman'}, 
-  {id: 23, name: 'Barbie Man'}, 
-  {id: 40, name: 'El gato con Guantes'},
-  {id: 40, name: 'El gato felix'}
-  ];
+  { id: 5, name: "Buzz MyYear" },
+  { id: 11, name: "Action Woman" },
+  { id: 23, name: "Barbie Man" },
+  { id: 40, name: "El gato con Guantes" },
+  { id: 40, name: "El gato felix" },
+];
 
-  const toysWithoutGato = [];
+const toysWithoutGato = [];
 
 for (const toy of toys) {
-    if (!toy.name.includes("gato")) 
-      toysWithoutGato.push(toy);
-      
-    
-  }
+  if (!toy.name.includes("gato")) toysWithoutGato.push(toy);
+}
 
 console.log(toysWithoutGato);
+
+
+
+
+/*----------------------------------------------------------------------------------
+-----------------------**Iteración #7: For...of avanzado**--------------------------
+------------------------------------------------------------------------------------*/
+
+
+/*Usa un bucle **for...of** para recorrer todos los juguetes y añade los que tengan más de 15 ventas (sellCount) al array popularToys.
+Imprimelo por consola.. Puedes usar este array:
+
+
+const popularToys = [];
+const toys = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+]
+*/
+
+const popularToys = [];
+const toys2 = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+];
+
+for (const toy2 of toys2) {
+  if (toy2.sellCount > 15){
+    popularToys.push(toy2);
+  }
+  
+}
+console.log(popularToys);
