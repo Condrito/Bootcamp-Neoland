c7e144d78845eadbe287676303df9332d2085877
Condrito <mareyeslacalle@gmail.com>
2022-10-14 19:23:05 +0200

    iteracion 2 Review Basics

--- Javascript/JS Review Basics/App.js
+++ Javascript/JS Review Basics/App.js
@@ -41,5 +41,97 @@ for (const categorie of listCategories) {
   if (!listCategoriesWithoutDuplicates.includes(categorie)) {
     listCategoriesWithoutDuplicates.push(categorie);
   }
-};
+}
 console.log(listCategoriesWithoutDuplicates);
+
+/*----------------------------------------------------------------------------------
+----------------------------**Iteración #2: Mix Fors**------------------------------
+------------------------------------------------------------------------------------*/
+
+/*Dado el siguiente javascript usa forof y forin para hacer la media del volumen de todos los sonidos favoritos que tienen los usuarios.
+
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+*/
+
+const users = [
+  {
+    name: "Manolo el del bombo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Mortadelo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Super Lopez",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "El culebra",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+let volumeList = [];
+
+for (const user of users) {
+  for (const key in user.favoritesSounds) {
+    const element = user.favoritesSounds[key].volume;
+    volumeList.push(element);
+  }
+}
+console.log(volumeList);
+
+let totalVolume = 0;
+
+for (const volume of volumeList) {
+  totalVolume += volume;
+}
+
+let averageVolume = totalVolume / volumeList.length;
+console.log(averageVolume);
