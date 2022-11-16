2fe5fb4d27d796d9c6758c7ecd9722bc19806531
Condrito <mareyeslacalle@gmail.com>
2022-11-16 21:01:43 +0100

    Vite-project/Color-Fliper

---
+++ Color-Flipper/main.js
@@ -0,0 +1,60 @@
+import './style.css'
+
+const COLOR_PALETTE = {
+  '#28262C': 'Raisin Black',
+  '#998FC7': 'Blue Bell',
+  '#D4C2FC': 'Lavender Blue',
+  '#F9F5FF': 'Magnolia',
+  '#14248A': 'Resolution Blue',
+  '#FA8072': 'Salmon',
+  '#808000': 'Olive',
+  '#00FF00': 'Lime',
+  '#00FFFF': 'Aqua'
+};
+
+Object.keys(COLOR_PALETTE).forEach((color)=>{
+  const option = document.createElement("option");
+  option.value = color;
+  option.innerText= COLOR_PALETTE[color];
+});
+
+
+
+const addOptionsToColorPicker = () => {
+  const colorPickerSelect = document.querySelector("#color-picker");
+
+Object.keys(COLOR_PALETTE).forEach((color)=>{
+  const option =document.createElement("option");
+  option.value=color;
+  option.innerText=COLOR_PALETTE[color];
+
+  colorPickerSelect.append(option);
+})
+}
+
+const addEventListenerToColorPicker =() => {
+  const colorPickerSelect = document.querySelector("#color-picker");
+  const colorName = document.querySelector("#color-name");
+
+  colorPickerSelect.addEventListener("change", (event)=>{
+
+    const newColor = event.target.value;
+
+    document.body.style.backgroundColor = newColor;
+
+    colorName.innerText = COLOR_PALETTE[newColor];
+
+  });
+};
+
+
+
+addOptionsToColorPicker();
+addEventListenerToColorPicker();
+
+
+const colorNameText = `${COLOR_PALETTE[newColor]} | ${newColor}` 
+    colorName.innerText = COLOR_PALETTE[newColor] ? colorNameText : "-";
+
+
+
